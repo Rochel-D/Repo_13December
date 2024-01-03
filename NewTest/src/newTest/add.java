858ae8be59a1e05a3114caf783d4031be9a83bfb
johannalvares1203 <johannalvares2003@gmail.com>
2024-01-03 14:10:18 +0530

    second commit

---
+++ NewTest/src/newTest/add.java
@@ -0,0 +1,14 @@
+package newTest;
+
+public class add {
+	int mult(int x, int y, int z) {
+		return x*y*z;
+	}
+	double divide(double a, double b) {
+		return a/b;
+	}
+	int add1(int e, int f) {
+		return e+f; 
+	}
+
+}
